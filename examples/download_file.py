65d5ba949f727f9b1c7a4268fe4844a77a68f2e2
JonStratton <jgstratt@sdf.org>
2020-01-01 21:23:43 -0600

    Some minor cleanup.

--- examples/download_file.py
+++ examples/download_file.py
@@ -1,36 +1,65 @@
-import base64
+import base64, getopt, sys
 from selenium import webdriver
 from selenium.webdriver.remote.file_detector import UselessFileDetector
 
-file_get = '/tmp/jonwashere.txt'
-local_file = './jonwashere.txt'
-
-driver = webdriver.Remote(
-   command_executor="http://selenium:4444/wd/hub",
-   desired_capabilities={"browserName": "firefox"}
-   )
-driver.file_detector = UselessFileDetector()
-
-try:
-    # A simple file input plus some FileReader js to store file contents in "inf"
-    driver.get('data:text/html;charset=utf-8,<html><input id=f type=file onchange="rf(event)"><script>var inf; var rf = function(e) { var inp = e.target; var read = new FileReader(); read.onload = function(){inf = read.result;}; read.readAsDataURL(inp.files[0]);}</script></html>')
+def get_file_contents(driver, remote_file):
+    " A simple file input plus some FileReader js to store file contents in 'inf' "
+    decoded_contents = None
     try:
-        driver.find_element_by_id('f').send_keys(file_get) # Load local file into input field (and therefor "inf")
+        driver.get('data:text/html;charset=utf-8,<html><input id=f type=file onchange="rf(event)"><script>var inf; var rf = function(e) { var inp = e.target; var read = new FileReader(); read.onload = function(){inf = read.result;}; read.readAsDataURL(inp.files[0]);}</script></html>')
+        driver.find_element_by_id('f').send_keys(remote_file) # Load local file into input field (and therefor "inf")
         js_return = driver.execute_script('return(inf)')   # Dump the contents of "inf"
         if js_return:
             try:
-                encoding, contents = js_return.split(',')
-                if local_file:
-                    f = open(local_file, 'w+b')
-                    f.write(base64.b64decode(contents))
-                    f.close()
-                else:
-                    print(base64.b64decode(contents))
+                decoded_contents = base64.b64decode(js_return.split(',')[1])
             except:
                 print('Cannot Decode: %s' % js_return)
         else:
-            print('Cannot Read: %s' % file_get)
+            print('Cannot Read: %s' % remote_file)
     except:
-        print('Doesnt Exist: %s' % file_get)
-finally:
-    driver.quit()
+        print('Doesnt Exist: %s' % remote_file)
+    return decoded_contents
+
+def write_local_file(local_file, file_contents):
+    " Simply write to a local binary file "
+    try:
+        f = open(local_file, 'w+b')
+        f.write(file_contents)
+        f.close()
+    except:
+        print('Cannot Write the following File: %s' % local_file)
+    return None
+
+def get_driver(url):
+    driver = webdriver.Remote(
+            command_executor=url,
+            desired_capabilities={'browserName': 'firefox'}
+            )
+    driver.file_detector = UselessFileDetector()
+    return driver
+
+def parse_args():
+    url_wd, remote_file, local_file = None, None, None
+    myopts, args = getopt.getopt(sys.argv[1:],':r:u:l:')
+    for o, a in myopts:
+        if o == '-u':
+            url_wd = a
+        elif o == '-r':
+            remote_file = a
+        elif o == '-l':
+            local_file = a
+
+    return url_wd, remote_file, local_file
+
+if __name__ == '__main__':
+    url_wd, remote_file, local_file = parse_args()
+    driver = get_driver(url_wd)
+    try:
+        file_contents = get_file_contents(driver, remote_file)
+        if local_file:
+            write_local_file(local_file, file_contents)
+        else:
+            print(file_contents.decode('ascii'))
+    finally:
+        driver.quit()
+    sys.exit(0)
--- examples/upload_file.py
+++ examples/upload_file.py
@@ -1,38 +1,59 @@
-import base64, os
+import base64, getopt, sys, os
 from selenium import webdriver
 from selenium.webdriver.remote.file_detector import UselessFileDetector
+# Cannot 1. Overwrite a file. 2. Upload a dotfile. 
+# Can create a directory
 
-local_file = '/bin/ls'
-remote_file = '/tmp/ls_bin'
+def get_driver(url, remote_path):
+    # To prevent download dialog
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('browser.download.folderList', 2) # custom location
+    profile.set_preference('browser.download.manager.showWhenStarting', False)
+    profile.set_preference('browser.download.dir', remote_path)
+    profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
 
-# Read in local file and base64 encode
-file_data = ''
-with open(local_file, 'rb') as file:
-    file_data = base64.b64encode(file.read())
+    options = webdriver.FirefoxOptions()
+    options.profile = profile
 
-# Break remote file into path and basename
-remote_path = os.path.dirname(remote_file)
-remote_basename = os.path.basename(remote_file)
+    driver = webdriver.Remote(
+            command_executor=url,
+            desired_capabilities=options.to_capabilities()
+            )
+    return driver
 
-data_url = 'data:text/html;charset=utf-8,<html><a id=f href="data:application/octet-stream;charset=utf-16le;base64,%s" download="%s">f</a></html>' % ( file_data.decode('utf-8'), remote_basename )
-print(data_url)
+def parse_args():
+    url_wd, remote_file, local_file, data = None, None, None, None
+    myopts, args = getopt.getopt(sys.argv[1:],':r:u:l:d:')
+    for o, a in myopts:
+        if o == '-u':
+            url_wd = a
+        elif o == '-r':
+            remote_file = a
+        elif o == '-l':
+            local_file = a
+        elif o == '-d':
+            #data = str.encode(a)
+            data = base64.b64encode(str.encode(a))
 
-# To prevent download dialog
-profile = webdriver.FirefoxProfile()
-profile.set_preference('browser.download.folderList', 2) # custom location
-profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', remote_path)
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
+    return url_wd, remote_file, local_file, data
 
-driver = webdriver.Remote(
-   command_executor="http://selenium:4444/wd/hub",
-   desired_capabilities={"browserName": "firefox"},
-   browser_profile=profile
-   )
-driver.file_detector = UselessFileDetector()
+if __name__ == '__main__':
+    url_wd, remote_file, local_file, data = parse_args()
 
-try:
-    driver.get(data_url)
-    driver.find_element_by_id('f').click()
-finally:
-    driver.quit()
+    # Just overwrite whatever is in data
+    if local_file:
+        with open(local_file, 'rb') as file:
+            data = base64.b64encode(file.read())
+
+    remote_path = os.path.dirname(remote_file)
+    remote_basename = os.path.basename(remote_file)
+
+    data_url = 'data:text/html;charset=utf-8,<html><a id=f href="data:application/octet-stream;charset=utf-16le;base64,%s" download="%s">f</a></html>' % ( data.decode('utf-8'), remote_basename )
+
+    driver = get_driver(url_wd, remote_path)
+    try:
+        driver.get(data_url)
+        driver.find_element_by_id('f').click()
+    finally:
+        driver.quit()
+    sys.exit(0)
